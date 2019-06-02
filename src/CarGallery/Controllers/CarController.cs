045915d4c95b24cd600697babbdaf12198ea40e2
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-06-02 08:02:21 +0100

    Car controller created.

---
+++ src/CarGallery/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using CarGallery.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarGallery.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly CarService _carService;
+
+        public CarController(CarService carService)
+        {
+            this._carService = carService;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View(this._carService.Get());
+        }
+    }
+}
--- src/CarGallery/Services/CarService.cs
+++ src/CarGallery/Services/CarService.cs
@@ -12,44 +12,44 @@ namespace CarGallery.Services
         private readonly string ConfigDatabaseName = "CarGalleryDb";
         private readonly string ConfigCarCollectionName = "Cars";
 
-        private readonly IMongoCollection<Car> cars;
+        private readonly IMongoCollection<Car> _cars;
 
         public CarService(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetConnectionString(this.ConfigConnectionString));
             var database = client.GetDatabase(this.ConfigDatabaseName);
-            this.cars = database.GetCollection<Car>(this.ConfigCarCollectionName);
+            this._cars = database.GetCollection<Car>(this.ConfigCarCollectionName);
         }
 
         public List<Car> Get()
         {
-            return this.cars.Find(c => true).ToList();
+            return this._cars.Find(c => true).ToList();
         }
 
         public Car Get(string id)
         {
-            return this.cars.Find(c => c.Id == id).FirstOrDefault();
+            return this._cars.Find(c => c.Id == id).FirstOrDefault();
         }
 
         public Car Create(Car car)
         {
-            this.cars.InsertOne(car);
+            this._cars.InsertOne(car);
             return car;
         }
 
         public void Update(string id, Car car)
         {
-            this.cars.ReplaceOne(c => c.Id == id, car);
+            this._cars.ReplaceOne(c => c.Id == id, car);
         }
 
         public void Remove(string id)
         {
-            this.cars.DeleteOne(c => c.Id == id);
+            this._cars.DeleteOne(c => c.Id == id);
         }
 
         public void Remove(Car car)
         {
-            this.cars.DeleteOne(c => c.Id == car.Id);
+            this._cars.DeleteOne(c => c.Id == car.Id);
         }
     }
 }
